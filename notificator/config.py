38b52221a241d833738cd96324f15283e82311c4
Ekaterina Frolova <ekaterina.frolova40@gmail.com>
2019-09-11 11:41:02 +0300

    Изменены переменные окружения и исправлены конфиги

--- notificator/config.py
+++ notificator/config.py
@@ -4,17 +4,17 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
 
-    SQLALCHEMY_BASE_URI = 'postgresql+psycopg2://{}:{}@{}/{}'.format(
-        'test', '1234', 'notificator.db', 'postgres')
+    SQLALCHEMY_BASE_URI = os.environ.get('SQLALCHEMY_BASE_URI') or \
+                          'postgresql+psycopg2://{}:{}@{}/{}'.format(
+                          'test', '1234', 'notificator.db', 'postgres')
 
-    LOGFILE = 'log.log'
-    MAIL_SERVER = 'smtp.yandex.ru'
-    MAIL_PORT = 587
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
     INTERVAL = (0,1)
     NOTIFICATION_TIME = os.environ.get('NOTIFICATION_TIME')
 
-    RECIPIENTS = ['ekaterina.frolova40@gmail.com',
-                  'katerinafr0lova@yandex.ru']
+    ADMIN = os.environ.get('ADMIN')
+    RECIPIENTS = [ADMIN, os.environ.get('RECIPIENT')]
